c1cbfec810333497da797ead6025c3698c4ad32c
ashleyromano <ashleye.romano@gmail.com>
2022-04-27 14:26:09 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+const kelvin = 0;
+//creating a constant variable
+
+const celsius = kelvin - 273;
+//creating a constant variable
+
+const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+//calculating fahrenheit
+
+//console.log(fahrenheit)
+
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit.')
+
